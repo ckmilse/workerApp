48e499592189025b5f8e1f21d45f8cf22ded262e
ck <153293261@qq.com>
2016-12-16 01:20:55 +0800

    修改

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-// app.use('/', index);
-app.use('/users', users);
 app.use(express.static(path.join(__dirname, 'public')));
+ app.use('/', index);
+app.use('/users', users);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
