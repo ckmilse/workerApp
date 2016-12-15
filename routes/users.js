75e87ef620a9d7716d4a07be7edda4694fa6add0
ck <153293261@qq.com>
2016-12-16 02:08:59 +0800

    修改routte

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 /* GET home page. */
 function indexController(req, res, next) {
-    var paths = filePaths.getSync('../controller/');
+    var paths = filePaths.getSync('./../controller/');
   // res.render('index', { title: 'Express' });
     res.send('respond with a resource' + paths.join(';'));
 }
