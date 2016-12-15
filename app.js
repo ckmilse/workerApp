c1e2300251cb70eb068a0129d27ae84cbb489f87
ck <153293261@qq.com>
2016-12-16 01:26:07 +0800

    增加zip插件

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //app.set('port', 80);
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(compression());
 app.use(bodyParser.json());
