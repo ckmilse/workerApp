e5af3326a2e1f121b308cc02c723c85998b8b1ff
ck <153293261@qq.com>
2016-12-16 02:01:59 +0800

    修改users

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # workerApp
 个人小项目
 希望没啥安全问题
+
+
+1.mark 别忘了ng的gzip;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var router = express.Router();
+var filePaths = require('filepaths');
+
+var fs = require('fs');
+
+/* GET home page. */
+function indexController(req, res, next) {
+    var paths = filePaths.getSync('../controller/');
+  // res.render('index', { title: 'Express' });
+    res.send('respond with a resource' + paths);
+}
+router.get('/', indexController);
 
 module.exports = router;
