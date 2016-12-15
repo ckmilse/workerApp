40aa7c3b6c2fdfae0c008bcb18f61d0dbbfc55f0
ck <153293261@qq.com>
2016-12-16 01:17:37 +0800

    修改

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.listen(80);
+app.listen(3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //app.set('port', 80);
@@ -22,11 +22,11 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+// app.use('/', index);
 app.use('/users', users);
-
+app.use(express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
