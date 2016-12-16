fadc26b1d7525c6b76d03f81f50c6e89e3edae2b
ck <153293261@qq.com>
2016-12-16 12:21:09 +0800

    console.log参数

--- controller/index.html.js
+++ controller/index.html.js
@@ -12,6 +12,7 @@ var fs = require('fs');
 // var paths = filePaths.getSync('app/scripts/');
 module.exports = function(req, res, next) {
   // res.send('respond with a resource3333' + JSON.stringify(req.body));
+  console.log(req);
   res.render('indexWorker', { title: 'Express' , bodyString: JSON.stringify(req.body)});
 };
 
