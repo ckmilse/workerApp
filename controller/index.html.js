efa294ec0bbc74c761264229a279499e115b7b96
ck <153293261@qq.com>
2016-12-16 11:42:16 +0800

    修改

---
+++ controller/index.html.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//     console.log(234);
+//   res.send('respond with a resource');
+// });
+
+var fs = require('fs');
+
+// var paths = filePaths.getSync('app/scripts/');
+module.exports = function(req, res, next) {
+  // res.send('respond with a resource3333' + JSON.stringify(req.body));
+  res.render('indexWorker', { title: 'Express' });
+};
+
+// module.exports = function() {
+//
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+
 var router = express.Router();
+var filePaths = require('filepaths');
+
+var fs = require('fs');
+var paths = filePaths.getSync('./controller/');
+var controllers = {};
+paths.forEach(function(item) {
+    var key = item.replace('./controller/','').replace('.js','');
+    controllers[key] = require('./.' + item);
+    router.get('/' + key, controllers[key]);
+    router.post('/' + key , controllers[key]);
+    console.log(controllers);
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// function indexController(req, res, next) {
+//
+//   // res.render('index', { title: 'Express' });
+//
+//     res.send('respond with a resource' + paths.join(';') + JSON.stringify(req.body) + req.url);
+// }
+// router.get('/', indexController);
+// router.post('/', indexController);
 
 module.exports = router;
