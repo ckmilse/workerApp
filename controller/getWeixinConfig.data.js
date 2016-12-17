d34e6bebec93d0915cf5b39da034564d47df06da
ck <153293261@qq.com>
2016-12-17 21:42:36 +0800

    修改

--- controller/getWeixinConfig.data.js
+++ controller/getWeixinConfig.data.js
@@ -13,17 +13,21 @@ var getSign = require('./../service/wx_jsTicket_sign.js');
 // var paths = filePaths.getSync('app/scripts/');
 module.exports = function(req, res, next) {
   // res.send('respond with a resource3333' + JSON.stringify(req.body));
-  var ulr = req.query.ajaxUrl;
-  var publicJson = getSign(global.worker_Conf.jsapi_ticket, 'http://www.whatsmax.com/index.html');
-  res.writeHeader(200, {
-      'Content-Type':'application/json;charset=UTF-8',
-      'Access-Control-Allow-Origin': '*.whatsmax.com'
-  })
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:63342');
+  var url = req.query.ajaxUr || 'http://www.whatsmax.com/index.html';
+  var publicJson = getSign(global.worker_Conf.jsapi_ticket, url);
+  if(publicJson.jsapi_ticket) {
+      delete publicJson.jsapi_ticket;
+  }
+  console.log(publicJson);
+  // res.writeHeader(200, {
+  //     'Content-Type':'application/json;charset=UTF-8',
+  //     'Access-Control-Allow-Origin': '*.whatsmax.com'
+  // })
+    res.header('Access-Control-Allow-Origin', '*.whatsmax.com');
     // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     // res.header('Access-Control-Allow-Headers', 'Content-Type');
     // res.header('Access-Control-Allow-Credentials','true');
-  res.send(JSON.stringify(req.publicJson));
+  res.send(JSON.stringify(publicJson));
 
 
 };
